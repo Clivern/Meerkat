a296a0335bfb1aad288f895db473625493628194
Clivern <hello@clivern.com>
2023-02-26 17:03:52 +0200

    Adjust profile, teams, users and settings pages

--- priv/static/theme/app/backend.js
+++ priv/static/theme/app/backend.js
@@ -176,10 +176,69 @@ scuti_app.profile_screen = (Vue, axios, $) => {
                     inputs[item.name] = item.value;
                 });
 
-                axios.post(_form.attr('action'), inputs)
+                axios.put(_form.attr('action'), inputs)
                     .then((response) => {
                         if (response.status >= 200) {
                             show_notification(response.data.successMessage);
+                        }
+                    })
+                    .catch((error) => {
+                        this.isInProgress = false;
+                        // Show error
+                        show_notification(error.response.data.errorMessage);
+                    });
+            }
+        }
+    });
+
+}
+
+// Profile Page
+scuti_app.profile_api_screen = (Vue, axios, $) => {
+
+    return new Vue({
+        delimiters: ['${', '}'],
+        el: '#app_api_key',
+        data() {
+            return {
+                apiKey: "*********************",
+                isInProgress: false,
+            }
+        },
+        methods: {
+            showApiKeyAction(event) {
+                event.preventDefault();
+                this.isInProgress = true;
+
+                axios.get(_globals.fetch_api_key_endpoint)
+                    .then((response) => {
+                        if (response.status >= 200) {
+                            this.apiKey = response.data.apiKey;
+                        }
+                    })
+                    .catch((error) => {
+                        this.isInProgress = false;
+                        // Show error
+                        show_notification(error.response.data.errorMessage);
+                    });
+            },
+
+            rotateApiKeyAction(event) {
+                event.preventDefault();
+                this.isInProgress = true;
+
+                let inputs = {};
+                let _self = $(event.target);
+                let _form = _self.closest("form");
+
+                _form.serializeArray().map((item, index) => {
+                    inputs[item.name] = item.value;
+                });
+
+                axios.put(_globals.rotate_api_key_endpoint, {})
+                    .then((response) => {
+                        if (response.status >= 200) {
+                            this.apiKey = response.data.apiKey;
                         }
                     })
                     .catch((error) => {
@@ -220,7 +279,7 @@ scuti_app.add_user_modal = (Vue, axios, $) => {
                 axios.post(_form.attr('action'), inputs)
                     .then((response) => {
                         if (response.status >= 200) {
-                            show_notification(i18n_globals.new_user);
+                            show_notification(_globals.new_user);
                             setTimeout(() => {
                                 location.reload();
                             }, 2000);
@@ -247,17 +306,13 @@ scuti_app.add_team_modal = (Vue, axios, $) => {
             return {
                 isInProgress: false,
                 users: [],
-                teamName: '',
-                teamSlug: ''
+                teamName: ''
             }
         },
         mounted() {
             this.loadData();
         },
         methods: {
-            slugifyTeamName() {
-                this.teamSlug = this.teamName.toLowerCase().replace(/\s+/g, '-');
-            },
             loadData() {
                 axios.get($("#add_team_modal").attr("data-action"), {
                         params: {
@@ -286,12 +341,12 @@ scuti_app.add_team_modal = (Vue, axios, $) => {
                     inputs[item.name] = item.value;
                 });
 
-                inputs["members"] = $("[name='members']").val();
+                inputs["members"] = $("form#add_team_form select[name='members']").val();
 
                 axios.post(_form.attr('action'), inputs)
                     .then((response) => {
                         if (response.status >= 200) {
-                            show_notification(i18n_globals.new_team);
+                            show_notification(_globals.new_team);
                             setTimeout(() => {
                                 location.reload();
                             }, 2000);
@@ -331,8 +386,35 @@ scuti_app.teams_list = (Vue, axios, $) => {
             }
         },
         methods: {
+            showTeamInfoAction(description) {
+                $("div#team_info_modal_content").text(description);
+            },
+
             editTeamAction(id) {
-                console.log("Edit team with ID:", id);
+                let current = $('form#update_team_form input[name="uuid"]').val()
+                if (current != "") {
+                    $('form#update_team_form').attr('action', function(i, val) {
+                        return val.replace(current, "UUID");
+                    });
+                }
+
+                $('form#update_team_form input[name="uuid"]').val(id);
+                $('form#update_team_form').attr('action', function(i, val) {
+                    return val.replace('UUID', id);
+                });
+
+                axios.get($("#update_team_form").attr("action"))
+                    .then((response) => {
+                        if (response.status >= 200) {
+                            $('form#update_team_form input[name="name"]').val(response.data.name);
+                            $('form#update_team_form input[name="slug"]').val(response.data.slug);
+                            $('form#update_team_form textarea[name="description"]').val(response.data.description);
+                            $('form#update_team_form select[name="members"]').val(response.data.members);
+                        }
+                    })
+                    .catch((error) => {
+                        show_notification(error.response.data.errorMessage);
+                    });
             },
 
             formatDatetime(datatime) {
@@ -340,14 +422,14 @@ scuti_app.teams_list = (Vue, axios, $) => {
             },
 
             deleteTeamAction(id) {
-                if (confirm(i18n_globals.delete_team_alert) != true) {
+                if (confirm(_globals.delete_team_alert) != true) {
                     return;
                 }
 
-                axios.delete(i18n_globals.delete_team_endpoint.replace("UUID", id), {})
+                axios.delete(_globals.delete_team_endpoint.replace("UUID", id), {})
                     .then((response) => {
                         if (response.status >= 200) {
-                            show_notification(i18n_globals.delete_team_message);
+                            show_notification(_globals.delete_team_message);
                             setTimeout(() => { location.reload(); }, 2000);
                         }
                     })
@@ -396,6 +478,71 @@ scuti_app.teams_list = (Vue, axios, $) => {
     });
 }
 
+scuti_app.edit_team_modal = (Vue, axios, $) => {
+
+    return new Vue({
+        delimiters: ['${', '}'],
+        el: '#edit_team_modal',
+        data() {
+            return {
+                isInProgress: false,
+                users: []
+            }
+        },
+        mounted() {
+            this.loadData();
+        },
+        methods: {
+            loadData() {
+                axios.get($("#edit_team_modal").attr("data-action"), {
+                        params: {
+                            offset: 0,
+                            limit: 10000
+                        }
+                    })
+                    .then((response) => {
+                        if (response.status >= 200) {
+                            this.users = response.data.users;
+                        }
+                    })
+                    .catch((error) => {
+                        show_notification(error.response.data.errorMessage);
+                    });
+            },
+            updateTeamAction(event) {
+                event.preventDefault();
+                this.isInProgress = true;
+
+                let inputs = {};
+                let _self = $(event.target);
+                let _form = _self.closest("form");
+
+                _form.serializeArray().map((item, index) => {
+                    inputs[item.name] = item.value;
+                });
+
+                inputs["members"] = $("form#update_team_form select[name='members']").val();
+
+                axios.put(_form.attr('action'), inputs)
+                    .then((response) => {
+                        if (response.status >= 200) {
+                            show_notification(_globals.update_team_message);
+
+                            setTimeout(() => {
+                                location.reload();
+                            }, 2000);
+                        }
+                    })
+                    .catch((error) => {
+                        this.isInProgress = false;
+                        // Show error
+                        show_notification(error.response.data.errorMessage);
+                    });
+            }
+        }
+    });
+}
+
 // Users list
 scuti_app.users_list = (Vue, axios, $) => {
 
@@ -420,7 +567,29 @@ scuti_app.users_list = (Vue, axios, $) => {
         },
         methods: {
             editUserAction(id) {
-                console.log("Edit user with ID:", id);
+                let current = $('form#update_user_form input[name="uuid"]').val()
+                if (current != "") {
+                    $('form#update_user_form').attr('action', function(i, val) {
+                        return val.replace(current, "UUID");
+                    });
+                }
+
+                $('form#update_user_form input[name="uuid"]').val(id);
+                $('form#update_user_form').attr('action', function(i, val) {
+                    return val.replace('UUID', id);
+                });
+
+                axios.get($("#update_user_form").attr("action"))
+                    .then((response) => {
+                        if (response.status >= 200) {
+                            $('form#update_user_form input[name="name"]').val(response.data.name);
+                            $('form#update_user_form select[name="role"]').val(response.data.role);
+                            $('form#update_user_form input[name="email"]').val(response.data.email);
+                        }
+                    })
+                    .catch((error) => {
+                        show_notification(error.response.data.errorMessage);
+                    });
             },
 
             formatDatetime(datatime) {
@@ -428,14 +597,14 @@ scuti_app.users_list = (Vue, axios, $) => {
             },
 
             deleteUserAction(id) {
-                if (confirm(i18n_globals.delete_user_alert) != true) {
+                if (confirm(_globals.delete_user_alert) != true) {
                     return;
                 }
 
-                axios.delete(i18n_globals.delete_user_endpoint.replace("UUID", id), {})
+                axios.delete(_globals.delete_user_endpoint.replace("UUID", id), {})
                     .then((response) => {
                         if (response.status >= 200) {
-                            show_notification(i18n_globals.delete_user_message);
+                            show_notification(_globals.delete_user_message);
                             setTimeout(() => { location.reload(); }, 2000);
                         }
                     })
@@ -485,6 +654,49 @@ scuti_app.users_list = (Vue, axios, $) => {
     });
 }
 
+scuti_app.edit_user_modal = (Vue, axios, $) => {
+
+    return new Vue({
+        delimiters: ['${', '}'],
+        el: '#edit_user_modal',
+        data() {
+            return {
+                isInProgress: false
+            }
+        },
+        methods: {
+            updateUserAction(event) {
+                event.preventDefault();
+                this.isInProgress = true;
+
+                let inputs = {};
+                let _self = $(event.target);
+                let _form = _self.closest("form");
+
+                _form.serializeArray().map((item, index) => {
+                    inputs[item.name] = item.value;
+                });
+
+                axios.put(_form.attr('action'), inputs)
+                    .then((response) => {
+                        if (response.status >= 200) {
+                            show_notification(_globals.update_user_message);
+
+                            setTimeout(() => {
+                                location.reload();
+                            }, 2000);
+                        }
+                    })
+                    .catch((error) => {
+                        this.isInProgress = false;
+                        // Show error
+                        show_notification(error.response.data.errorMessage);
+                    });
+            }
+        }
+    });
+}
+
 $(document).ready(() => {
     axios.defaults.headers.common = {
         'X-Requested-With': 'XMLHttpRequest',
@@ -523,8 +735,32 @@ $(document).ready(() => {
         );
     }
 
+    if (document.getElementById("app_api_key")) {
+        scuti_app.profile_api_screen(
+            Vue,
+            axios,
+            $
+        );
+    }
+
     if (document.getElementById("add_user_modal")) {
         scuti_app.add_user_modal(
+            Vue,
+            axios,
+            $
+        );
+    }
+
+    if (document.getElementById("edit_user_modal")) {
+        scuti_app.edit_user_modal(
+            Vue,
+            axios,
+            $
+        );
+    }
+
+    if (document.getElementById("edit_team_modal")) {
+        scuti_app.edit_team_modal(
             Vue,
             axios,
             $
