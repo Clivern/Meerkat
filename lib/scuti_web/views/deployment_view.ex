1ab0656cdaf943a366a564c8c5228803fc29b4fd
Clivern <hello@clivern.com>
2023-03-12 23:13:56 +0100

    updates

--- lib/scuti_web/views/deployment_view.ex
+++ lib/scuti_web/views/deployment_view.ex
@@ -10,7 +10,7 @@ defmodule ScutiWeb.DeploymentView do
   # Render deployments list
   def render("list.json", %{deployments: deployments, metadata: metadata}) do
     %{
-      deployments: Enum.map(deployments, &render_group/1),
+      deployments: Enum.map(deployments, &render_deployment/1),
       _metadata: %{
         limit: metadata.limit,
         offset: metadata.offset,
@@ -21,7 +21,7 @@ defmodule ScutiWeb.DeploymentView do
 
   # Render deployment
   def render("index.json", %{deployment: deployment}) do
-    render_group(deployment)
+    render_deployment(deployment)
   end
 
   # Render errors
@@ -30,7 +30,7 @@ defmodule ScutiWeb.DeploymentView do
   end
 
   # Format deployment
-  defp render_group(deployment) do
+  defp render_deployment(deployment) do
     {_, team} = TeamModule.get_team_by_id(deployment.team_id)
 
     %{
@@ -41,19 +41,19 @@ defmodule ScutiWeb.DeploymentView do
         id: team.uuid,
         name: team.name
       },
-      hosts_filter: deployment.hosts_filter,
-      host_groups_filter: deployment.host_groups_filter,
-      patch_type: deployment.patch_type,
-      pre_patch_script: deployment.pre_patch_script,
-      patch_script: deployment.patch_script,
-      post_patch_script: deployment.post_patch_script,
-      post_patch_reboot_option: deployment.post_patch_reboot_option,
-      rollout_strategy: deployment.rollout_strategy,
-      rollout_strategy_value: deployment.rollout_strategy_value,
-      schedule_type: deployment.schedule_type,
-      schedule_time: deployment.schedule_time,
-      last_status: deployment.last_status,
-      last_run_at: deployment.last_run_at,
+      hostsFilter: deployment.hosts_filter,
+      hostGroupsFilter: deployment.host_groups_filter,
+      patchType: deployment.patch_type,
+      prePatchScript: deployment.pre_patch_script,
+      patchScript: deployment.patch_script,
+      postPatchScript: deployment.post_patch_script,
+      postPatchRebootOption: deployment.post_patch_reboot_option,
+      rolloutStrategy: deployment.rollout_strategy,
+      rolloutStrategyValue: deployment.rollout_strategy_value,
+      scheduleType: deployment.schedule_type,
+      scheduleTime: deployment.schedule_time,
+      lastStatus: deployment.last_status,
+      lastRunAt: deployment.last_run_at,
       createdAt: deployment.inserted_at,
       updatedAt: deployment.updated_at
     }
